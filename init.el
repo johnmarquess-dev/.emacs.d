b63313c3d8986831e4ba0b872d05d92f9202cfca
johnmarquess <john@marquess.me>
2020-02-13 21:28:08 +1000

    added an emacs .gitignore

---
+++ init.el
@@ -0,0 +1,11 @@
+;; [[file:~/.emacs.d/init.org::*Some%20initial%20settings][Some initial settings:1]]
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+;; Some initial settings:1 ends here
+
+;; [[file:~/.emacs.d/init.org::*UI%20Tweaks][UI Tweaks:1]]
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+;; UI Tweaks:1 ends here
