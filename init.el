943e1f84ed658c5b42b8bca0b6b985f2782d6d5d
johnmarquess <john@marquess.me>
2020-02-14 11:41:43 +1000

    Added magit

--- init.el
+++ init.el
@@ -8,7 +8,11 @@
 (tool-bar-mode -1)
 ;; Menu-bar:1 ends here
 
-;; [[file:~/.emacs.d/init.org::*Install%20use-package][Install use-package:1]]
+;; [[file:~/.emacs.d/init.org::*Split%20window%20horizontally][Split window horizontally:1]]
+(split-window-horizontally)
+;; Split window horizontally:1 ends here
+
+;; [[file:~/.emacs.d/init.org::*Set%20up%20package%20archives%20and%20install%20use-package][Set up package archives and install use-package:1]]
 (require 'package)
 (add-to-list 'package-archives
        '("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -22,22 +26,11 @@
   (package-install 'use-package ))
 
 (require 'use-package)
-;; Install use-package:1 ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((eval add-hook
-	   (quote after-save-hook)
-	   (lambda nil
-	     (org-babel-tangle))
-	   nil t)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Set up package archives and install use-package:1 ends here
+
+;; [[file:~/.emacs.d/init.org::*Intsall%20magit][Intsall magit:1]]
+(use-package magit
+  :ensure t
+  :commands (magit-status)
+  :bind ("C-x g" . magit-status))
+;; Intsall magit:1 ends here
