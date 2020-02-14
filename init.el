76329bf5f46e75d3bfd67cf1f8e585a1aeeac7a1
johnmarquess <john@marquess.me>
2020-02-14 12:11:36 +1000

    amended use package config

--- init.el
+++ init.el
@@ -28,9 +28,30 @@
 (require 'use-package)
 ;; Set up package archives and install use-package:1 ends here
 
+;; [[file:~/.emacs.d/init.org::*Configure%20use-package][Configure use-package:1]]
+(setq use-package-always-ensure t)
+;; Configure use-package:1 ends here
+
 ;; [[file:~/.emacs.d/init.org::*Intsall%20magit][Intsall magit:1]]
 (use-package magit
-  :ensure t
   :commands (magit-status)
   :bind ("C-x g" . magit-status))
 ;; Intsall magit:1 ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((eval add-hook
+	   (quote after-save-hook)
+	   (lambda nil
+	     (org-babel-tangle))
+	   nil t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
